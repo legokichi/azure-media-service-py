cfd9722bdbc150f3fa455e138d83821eabb5947e
legokichi <legokichi@gmail.com>
2017-12-06 10:53:15 +0900

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+## Use this for Azure AD authentication
+from msrestazure.azure_active_directory import AADTokenCredentials
+
+## Required for Azure Data Lake Store account management
+from azure.mgmt.datalake.store import DataLakeStoreAccountManagementClient
+from azure.mgmt.datalake.store.models import DataLakeStoreAccount
+
+## Required for Azure Data Lake Store filesystem management
+from azure.datalake.store import core, lib, multithread
+
+# Common Azure imports
+import adal
+from azure.mgmt.resource.resources import ResourceManagementClient
+from azure.mgmt.resource.resources.models import ResourceGroup
+
+## Use these as needed for your application
+import logging, getpass, pprint, uuid, time
---
+++ readme.md
@@ -0,0 +1,4 @@
+pyenv shell anaconda3-4.1.1
+pip install pipenv
+pipenv --three
+pipenv install azure-mgmt-resource azure-mgmt-datalake-store azure-datalake-store
